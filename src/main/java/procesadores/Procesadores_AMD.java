50b59022798e83fecbb558b800b8e33aafc8c308
Juan Zaldumbide <jzaldumbide@gmail.com>
2024-05-30 08:19:09 -0500

    tercer commit, implementadatada las clases Procesador_Intel y Procesador_AMD

---
+++ src/main/java/procesadores/Procesadores_AMD.java
@@ -0,0 +1,19 @@
+package procesadores;
+
+public class Procesadores_AMD extends Procesadores_escritorio{
+    Boolean sensor_temperatura;
+    public Procesadores_AMD() {}
+
+    public Procesadores_AMD(int nucleos, Double consumo, Double velocidad, String nombre, String socket) {
+        super(nucleos, consumo, velocidad, nombre, socket);
+        this.sensor_temperatura=sensor_temperatura;
+    }
+
+    public Boolean getSensor_temperatura() {
+        return sensor_temperatura;
+    }
+
+    public void setSensor_temperatura(Boolean sensor_temperatura) {
+        this.sensor_temperatura = sensor_temperatura;
+    }
+}
---
+++ src/main/java/procesadores/Procesadores_Intel.java
@@ -0,0 +1,25 @@
+package procesadores;
+
+public class Procesadores_Intel extends Procesadores_escritorio{
+    Boolean tarjeta_video;
+
+    public Procesadores_Intel() {
+    }
+
+    public Procesadores_Intel(int nucleos, Double consumo, Double velocidad, String nombre, String socket, Boolean tarjeta_video) {
+        super(nucleos, consumo, velocidad, nombre, socket);
+        this.tarjeta_video = tarjeta_video;
+    }
+
+    public Boolean getTarjeta_video() {
+        return tarjeta_video;
+    }
+
+    public void setTarjeta_video(Boolean tarjeta_video) {
+        this.tarjeta_video = tarjeta_video;
+    }
+
+    public void imprimir_datos_Intel(){
+        System.out.println(this.getTarjeta_video());
+    }
+}
