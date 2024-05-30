8187ea767acc6a266de215bf98f4b1415fccfdeb
Ithan <ithan.camacho@epn.edu.ec>
2024-05-30 10:01:03 -0500

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import procesadores.Procesadores;
 import procesadores.Procesadores_Intel;
 import procesadores.Procesadores_AMD;
+import procesadores.Procesadores_moviles;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,6 +30,10 @@ public class Main {
         Procesadores_AMD proc_1 = new Procesadores_AMD(3,10.5,18, "Ryzen9","AM5",false);
         Procesadores_AMD proc_2 = new Procesadores_AMD(8,12.5,10, "Epy77","FM1",false);
         Procesadores_AMD proc_3 = new Procesadores_AMD(4,13,5, "Threadripper","B650",true);
+
+        //Ithan Camacho
+        Procesadores_moviles quealcom = new Procesadores_moviles(8, 6.0, 12.6, "Snapdragon 8 gen2", true);
+
     }
 
 }
