1017b5232d636466b15662abf597e97ee6b46944
1-21-01-E043-02\POO <mireya.garcia@epn.edu.ec>
2024-05-30 09:51:57 -0500

    Version mireya garcia

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
 
 import procesadores.Procesadores;
-
+import procesadores.Procesadores_Intel;
 
 public class Main {
     public static void main(String[] args) {
 
         Procesadores p1 = new Procesadores();
+        //Cambio Mireya García
+        Procesadores_Intel pro_3 = new Procesadores_Intel(5,7.5,5, "Corei7","LGA1158",true);
+        Procesadores_Intel pro_4 = new Procesadores_Intel(4,4.5,8, "Corei8","LGA1159",true);
+        Procesadores_Intel pro_5 = new Procesadores_Intel(6,6.5,4, "Corei4","LGA1153",false);
 
     }
 }
