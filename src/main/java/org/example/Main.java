59a365fa6ee6b908b77d4da18a9bb523473f364a
mateoxtra <mateoextra93@gmail.com>
2024-05-30 09:56:23 -0500

    Rama mateo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import procesadores.Procesadores_moviles;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Mateo Moran
         Procesadores_Intel procesador1 = new Procesadores_Intel(8,65.5,3.5,"Intel i7 11700","LGA 1152",true);
         Procesadores_AMD procesador2 = new Procesadores_AMD(4,40.5,2.5,"AMD Ryzen 7 5700G","AM4");
         Procesadores_moviles movil1 = new Procesadores_moviles(2,2.5,3.5,"iPhone 11",true);
