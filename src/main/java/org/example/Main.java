e16a52eeb6801dcc01b269ec12ec69e237bedf0c
JoseEduardo23 <jose.lincango@epn.edu.ec>
2024-05-30 09:59:35 -0500

    Iteracion AMD JOSE LINCANGO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import procesadores.Procesadores_AMD;
 import procesadores.Procesadores;
 import procesadores.Procesadores_AMD;
 import procesadores.Procesadores_moviles;
@@ -18,6 +18,31 @@ public class Main {
         //Adrian Ramos
         Procesadores_moviles quealcom = new Procesadores_moviles(8, 6.3, 12.6, "Snapdragon 8 gen3", true);
 
+<<<<<<< HEAD
+        //Richard Soria
+
+        // Walter Cobacango
+
+        //Elkin Diaz
+    }
+    public static void main(String[] args) {
+
+        Procesadores pK = new Procesadores();
+        //Kevin Mendoza integrante sto dgo
+
+
+        // Jose Lincango Instancias
+        Procesadores_AMD proc_900 = new Procesadores_AMD(4,4.5,6.5,"Intel AMD S5","socket ssd");
+
+
+
+
+
+
+
+
+
+        //Josue Guerra
         // Carlos Perez
         Procesadores_AMD procesador1 = new Procesadores_AMD(6, 65.0, 3.5, "AMD RYZEN5 5600X", "AM4");
     }
