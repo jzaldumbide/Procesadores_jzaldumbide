f31eb2ef0f1751a772eeea7f052e80ae68bef251
1-21-01-E043-28\POO <john.cobacango@epn.edu.ec>
2024-05-30 10:01:06 -0500

    WALTER COBACANGO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,4 +21,17 @@ public class Main {
         // Carlos Perez
         Procesadores_AMD procesador1 = new Procesadores_AMD(6, 65.0, 3.5, "AMD RYZEN5 5600X", "AM4");
     }
+
+
+
+
+
+
+    // Walter Cobacango
+    Procesadores_AMD prec1 = new Procesadores_AMD(12, 105.0, 4.8, "AMD Ryzen 9 5900X", "AM4");
+    Procesadores_AMD prec2 = new Procesadores_AMD(8, 105.0, 4.7, "AMD Ryzen 7 5800X", "Socket AM4");
+    Procesadores_AMD prec3 = new Procesadores_AMD(6, 65.0, 4.6, "AMD Ryzen 5 5600X", "Socket AM4");
+
+
+
 }
