1a0157e3158424e38fff941a321e278aab05a29b
stalinsangucho <stalinsangucho87@gmail.com>
2024-05-30 10:02:39 -0500

    Instancia David

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,6 +17,12 @@ public class Main {
 
         //Alisson Muñoz
 
+        //David Muela
+        Procesadores_AMD amd1 = new Procesadores_AMD(12,65.00,3.5,"Ryzer 3 3200G","AMD4");
+        Procesadores_AMD amd2 = new Procesadores_AMD(8,35.00,2.5,"Ryzer 5 5600G","AMD4");
+        Procesadores_AMD amd3 = new Procesadores_AMD(16,85.00,4.5,"Ryzer 5 5600X","AMD4")
+
+
 
 
 
