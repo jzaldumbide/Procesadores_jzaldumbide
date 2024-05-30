e4f78a6e0489007ed7fb9905ceced8d6ccaeffcb
stalinsangucho <stalinsangucho87@gmail.com>
2024-05-30 09:52:58 -0500

    Instancia David

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import procesadores.Procesadores;
+import procesadores.Procesadores_AMD;
 
 
 public class Main {
     public static void main(String[] args) {
-
-        Procesadores p1 = new Procesadores();
+        //David Muela
+        Procesadores_AMD amd1 = new Procesadores_AMD(12,65.00,3.5,"Ryzer 3 3200G","AMD4");
+        Procesadores_AMD amd2 = new Procesadores_AMD(8,35.00,2.5,"Ryzer 5 5600G","AMD4");
+        Procesadores_AMD amd3 = new Procesadores_AMD(16,85.00,4.5,"Ryzer 5 5600X","AMD4")
 
     }
 }
